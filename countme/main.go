e6a5c8012fa1b21c3030635edc3a4083999dd188
Mahdi Zareie <mahdi.elf@gmail.com>
2020-04-28 14:40:40 +0430

    Remove the lock

--- countme/main.go
+++ countme/main.go
@@ -6,42 +6,26 @@ import (
 	"log"
 	"regexp"
 	"strconv"
-	"sync"
 )
 
-type SafeCounter struct {
-	v   int64
-	mux sync.Mutex
-}
-
-func (c *SafeCounter) Inc(value int64) {
-	c.mux.Lock()
-	c.v += value
-	c.mux.Unlock()
-}
-
-func (c *SafeCounter) Get() int64 {
-	c.mux.Lock()
-	defer c.mux.Unlock()
-	return c.v
-}
-
-var x = SafeCounter{}
+var x int64
 
 type echoServer struct {
 	*gnet.EventServer
 }
+
 var RES = ([]byte)("HTTP/1.1 200 OK\n\n")
 var REG = regexp.MustCompile("\\D+")
+
 func (es *echoServer) React(frame []byte, c gnet.Conn) (out []byte, action gnet.Action) {
 	rawReq := (string)(frame)
 	if rawReq[0:1] == "G" {
-		out = ([]byte)(fmt.Sprintf("HTTP/1.1 200 OK\n\n%d\r\n", x.Get()))
+		out = ([]byte)(fmt.Sprintf("HTTP/1.1 200 OK\n\n%d\r\n", x))
 		_ = c.Close()
 		return
 	}
 	result, _ := strconv.ParseInt(REG.ReplaceAllString(rawReq[len(rawReq)-10:], ""), 10, 64)
-	x.Inc(result)
+	x += result
 	out = RES
 	_ = c.Close()
 	return
@@ -49,6 +33,5 @@ func (es *echoServer) React(frame []byte, c gnet.Conn) (out []byte, action gnet.
 
 func main() {
 	echo := new(echoServer)
-	log.Fatal(gnet.Serve(echo, "tcp://:80" ))
+	log.Fatal(gnet.Serve(echo, "tcp://:8080"))
 }
-
